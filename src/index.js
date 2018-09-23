245bfc6b491112f0d9867f167800a56005cf0f66
Disa0808 <levshukdenis@mail.ru>
2018-09-23 14:51:31 +0300

    Completely resolved

--- src/index.js
+++ src/index.js
@@ -3,19 +3,44 @@ module.exports = function longestConsecutiveLength(array) {
     return a - b;
   });
   var longestSequence = 0;
-  var tmpSequence = 0;
+  var tmpSequence = 1;
   
-  for(var i = 1; i < sortedArray.length; i++) {
-    if(sortedArray[i] - sortedArray[i-1] == 1){
-      tmpSequence++;
-      continue;
-    } else {
-      if(tmpSequence > longestSequence) {
-        longestSequence = tmpSequence;
-      }
-      tmpSequence = 1;
-    }
-  } 
+  // sortedArray.map(function(itm, inx, arr){
+  //   if(itm - arr[inx] == 1){
+  //     tmpSequence++;
+  //   } else {
+  //     if(tmpSequence > longestSequence) {
+  //       longestSequence = tmpSequence;
+  //     }
+  //     tmpSequence = 1;
+  //   }
+  // });
+
+  switch(+sortedArray.length){
+    case 0:
+      longestSequence = 0;
+      break;
+    case 1:
+      longestSequence = 1;
+      break;
+    default:
+      for(var i = 1; i < sortedArray.length; i++) {
+        if(sortedArray[i] - sortedArray[i-1] == 1){
+          tmpSequence++;
+          continue;
+        } 
+        if(sortedArray[i] === sortedArray[i-1]){
+          continue;
+        } else {
+          if(tmpSequence > longestSequence) {
+            longestSequence = tmpSequence;
+          }
+          tmpSequence = 1;
+        }
+      } 
+  }
+
+
   return longestSequence;
 
 }
